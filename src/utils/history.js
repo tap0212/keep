091c4745316a2af98c25e2c3624696a5a3ecc845
tapan021299 <77850787+tapan021299@users.noreply.github.com>
2021-04-25 13:41:02 +0530

    fixed routing removed github pages

--- src/utils/history.js
+++ src/utils/history.js
@@ -1,4 +1,3 @@
 import { createBrowserHistory } from 'history';
-const baseUrl = process.env.NODE_ENV === 'production' ? '/keep' : '/';
-const history = createBrowserHistory({ basename: baseUrl });
+const history = createBrowserHistory();
 export default history;
